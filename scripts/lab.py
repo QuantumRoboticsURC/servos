cab904b1304dbe3b48fbaff00c8d4caace9b6187
iker <A01749675@tec.mx>
2023-02-27 14:19:09 -0600

    joint3

--- scripts/lab.py
+++ scripts/lab.py
@@ -4,10 +4,12 @@ from adafruit_servokit import ServoKit
 import time
 
 def callback():
+    global servo_center,servo_left,servo_right,joint3_b
     lab = arm_lab()
     kit.servo[servo_left].angle=lab.servo1
     kit.servo[servo_center].angle=lab.servo2
     kit.servo[servo_right].angle=lab.servo3
+    kit.servo[joint3_b].angle=lab.joint3
 
 def servo_listener():
     rospy.init_node('lab_servos',anonymous=True)
@@ -19,11 +21,14 @@ if __name__ == '__main__':
     servo_left = 4
     servo_center = 5
     servo_right = 6
+    joint3_b = 7
     kit.servo[servo_left].actuation_range=180
     kit.servo[servo_center].actuation_range=180
     kit.servo[servo_right].actuation_range=180
+    kit.servo[joint3_b].actuation_range=642
     kit.servo[servo_left].set_pulse_width_range(1000,2000)
     kit.servo[servo_center].set_pulse_width_range(1000,2000)
     kit.servo[servo_right].set_pulse_width_range(1000,2000)
+    kit.servo[joint3_b].set_pulse_width_range(500,2500)
 
     servo_listener()
